0ef73dfae6d73b4a7d373c2d2fe6192c15c45d67
animalise <72106785+animalise@users.noreply.github.com>
2021-01-04 19:43:30 -0500

    add date & time format

--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 function formatDate(timestamp){
+
   let date = new Date(timestamp);
+
   let hours = date.getHours();
+  if (hours > 12) {
+    hours = hours - 12;
+  } 
+  if (hours < 1) {
+    hours = hours + 12;
+  } 
   if (hours < 10) {
    hours = `0${hours}`;
   }
+
   let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`; 
   }
 
+  console.log(hours);
+  
   let days = ["Sunday", "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday"];
   let day = days[date.getDay()];
   
-  let months = ["Jan." , "Feb." , "Mar." , "Apr." , "May" , "Jun." , "Jul." , "Aug." , "Sept." , "Oct." , "Nov." , "Dec."]
+  let months = ["Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug" , "Sept" , "Oct" , "Nov" , "Dec"]
   let month = months[date.getMonth()];
 
-  return `${date};`
+  return `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()} <br  /> ${date.getHours()}:${date.getMinutes()}`;
 }
 
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector(".current-temp");
   let cityElement = document.querySelector(".city");
   let descriptionElement = document.querySelector(".current-atmosphere");
@@ -28,11 +38,11 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = Math.round(response.data.main.temp); 
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
-  dateElement.innerHTML = formatDate(response.data.dt * 1000)
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "af173d370d3263e90c511e8cd78a494a";
-let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=New%20York&appid=${apiKey}&units=imperial`;
+let city = "Hong Kong"
+let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
-
